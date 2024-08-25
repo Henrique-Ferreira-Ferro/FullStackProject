0f40a80af33646b198a4626b14707000074c389c
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-08-25 17:30:03 -0300

    Modificação na controller

--- src/main/java/br/com/criandoApi/projeto/controller/UsuarioController.java
+++ src/main/java/br/com/criandoApi/projeto/controller/UsuarioController.java
@@ -1,12 +1,15 @@
 package br.com.criandoApi.projeto.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.criandoApi.projeto.model.Usuario;
@@ -65,6 +69,20 @@ public class UsuarioController {
 		return ResponseEntity.status(200).build();
 	}
 	
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public Map<String, String> handleValidationException(MethodArgumentNotValidException ex){
+		
+		Map<String, String> errors = new HashMap<>();
+		
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
+		
+	}
 	
 	
 	
