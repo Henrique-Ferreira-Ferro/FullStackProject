d012b8cdb26bd4c42092ad041acbf4134f5cfca6
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-09-12 09:57:33 -0300

    Criação da parte de segurança da aplicação

---
+++ src/main/java/br/com/criandoApi/projeto/dto/LoginRequestDTO.java
@@ -0,0 +1,18 @@
+package br.com.criandoApi.projeto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequestDTO {
+	
+	private String email;
+	private String senha;
+	
+
+}
---
+++ src/main/java/br/com/criandoApi/projeto/dto/RegisterRequestDTO.java
@@ -0,0 +1,21 @@
+package br.com.criandoApi.projeto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequestDTO {
+	
+	private String nome;
+	private String username;
+	private String email;
+	private String senha;
+	private String telefone;
+	
+	
+}
