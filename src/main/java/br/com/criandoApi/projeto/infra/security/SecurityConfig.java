8fd416c7ef64bc3be9fe1d5bfa0d47a7f0ca0db8
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-09-19 17:17:21 -0300

    Aplicando as configurações do swagger no projeto

--- src/main/java/br/com/criandoApi/projeto/infra/security/SecurityConfig.java
+++ src/main/java/br/com/criandoApi/projeto/infra/security/SecurityConfig.java
@@ -31,6 +31,8 @@ public class SecurityConfig {
 	                .authorizeHttpRequests(authorize -> authorize
 	                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
 	                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+	                        .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+	                        .requestMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
 	                        .requestMatchers(HttpMethod.GET,"/usuarios/all").hasAuthority("ADMIN")
 	                        .anyRequest().authenticated()
 	                )
